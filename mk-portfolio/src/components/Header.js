23f8262932e15e4c0dcfd79958507568a95a9cb7
kellymedia <mk@uconn.edu>
2020-09-21 20:11:07 -0400

    format Header components, remove unnecessary line spacing

--- mk-portfolio/src/components/Header.js
+++ mk-portfolio/src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   const [click, setClick] = useState(false);
@@ -22,54 +22,50 @@ function Header() {
     showButton();
   }, []);
 
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             MK
           </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className="nav-item">
               <Link
-                to='/projects'
-                className='nav-links'
+                to="/projects"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Projects
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/about'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
                 About
               </Link>
             </li>
 
             <li>
               <Link
-                to='/contact'
-                className='nav-links-mobile contact-button'
+                to="/contact"
+                className="nav-links-mobile contact-button"
                 onClick={closeMobileMenu}
               >
                 Contact
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>Contact</Button>}
+          {button && <Button buttonStyle="btn--outline">Contact</Button>}
         </div>
       </nav>
     </>
